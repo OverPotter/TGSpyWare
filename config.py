aea9d9962f1cc92f8597cf702c7c255d3e5403b4
Qd Miracle <qdmiracle@mail.ru>
2023-01-03 21:52:05 +0300

    add temp directory

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 TEMP_DIR = tempfile.gettempdir()
 
 AUTORUN_NAME = "WebDrive"
-RUN_FILENAME = "main.py"
+RUN_FILENAME = "start.bat"
 
 RECORD_PATH = os.path.join(TEMP_DIR, "sound.wav")
 SCREENSHOT_PATH = os.path.join(TEMP_DIR, "screenshot.jpg")
