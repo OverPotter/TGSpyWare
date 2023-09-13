2d672670d9d197150045048c97696d9f1f3d932b
Qd Miracle <qdmiracle@mail.ru>
2023-09-13 14:07:24 +0300

    added Disclaimer

--- modules/cmd_shell.py
+++ modules/cmd_shell.py
@@ -6,5 +6,12 @@ class Shell:
         pass
 
     @staticmethod
-    def exec_command(command):
-        subprocess.call(command)
+    def run_something(something: str) -> str:
+        """
+        Disclaimer:
+        This module is used exclusively for educational purposes.
+        I do not support and condemn any cybercrime.
+
+        Remember about escaping characters.
+        """
+        return subprocess.check_output(something).decode("cp866", "ignore")
